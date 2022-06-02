719a33979c8131d857c39d333d1a4a1d9b26a4ca
Frode Solheim <frode@solheim.dev>
2022-06-02 20:11:50 +0200

    Added libslirp

--- deps.sh
+++ deps.sh
@@ -6,6 +6,7 @@ fsdeps/packages/libfluidsynth
 fsdeps/packages/libmt32emu
 fsdeps/packages/libopusfile
 fsdeps/packages/libpng
+fsdeps/packages/libslirp
 fsdeps/packages/SDL2
 fsdeps/packages/SDL2_net
 fsdeps/packages/zlib
